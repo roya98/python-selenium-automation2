f12112a9540bcc1c3e236c0bbd6fb64137468673
Roya Amjadi <roya.torshizi.com>
2024-05-02 19:57:09 -0700

    updated sample_script and added sample_2

---
+++ sample_2.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+driver_path = ChromeDriverManager().install()
+
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+driver.get("https://www.google.com/")
